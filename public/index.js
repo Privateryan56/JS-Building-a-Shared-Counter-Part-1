2aaaee3472d216e427390f3abf5442d470617a54
Austin bambino <link9215@gmail.com>
2022-02-19 13:46:32 -0500

    part 1 and 2

--- public/index.js
+++ public/index.js
@@ -2,17 +2,45 @@ function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+        async function getIP(){
+            let newCount = await fetch('http://localhost:9001/counter');
+           const newCountResponse = await newCount.json();
+            console.log(newCountResponse.value);
+            return newCountResponse;
+        }
+        getIP().then(function(newCountResponse){
+           countContainer.textContent = newCountResponse.value;
+           countValue = newCountResponse.value;
 
-    let countValue = 0;
+        })
+    let countValue;
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
